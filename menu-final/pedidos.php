150cb898b62c3226a7c9bb42feeb3d65f6ec6d49
Delgao11 <91954337+Delgao11@users.noreply.github.com>
2023-06-01 14:26:12 +0200

    Update pedidos.php

--- menu-final/pedidos.php
+++ menu-final/pedidos.php
@@ -17,50 +17,48 @@
             <div id="pedidos-realizados" class="pedidos-realizados">
                 <a href="javascript:history.back()" class="boton-boton-secundario" id="volverBtn">Volver</a>
                 <?php
-                    require('conexion.php');
-                    session_start();
+require('conexion.php');
+session_start();
 
-                    $response = ""; // Variable para almacenar la respuesta
+$response = ""; // Variable para almacenar la respuesta
 
-                    // Obtener el código de mesa de la sesión
-                    $codigoMesa = $_SESSION["codigo_mesa"];
+// Obtener el código de mesa de la sesión
+$codigoMesa = $_SESSION["codigo_mesa"];
 
-                    // Verificar si el usuario es un administrador
-                    if ($codigoMesa == "admin") {
-                        echo '<p class="ningun-pedido">No tienes ningún pedido reciente</p>';
-                    } else {
-                        // Obtener los pedidos realizados por el usuario actual
-                        $stmt = $conn->prepare("SELECT * FROM pedidos WHERE codigo_mesa = ? AND TIMESTAMPDIFF(MINUTE, fecha_hora, NOW()) <= 60");
-                        if ($stmt) {
-                            $stmt->bind_param("s", $codigoMesa);
-                            $stmt->execute();
-                            $result = $stmt->get_result();
-                            $pedidos = $result->fetch_all(MYSQLI_ASSOC);
-                            $stmt->close();
-                        } else {
-                            $response = "Error al preparar la consulta: " . $conn->error;
-                        }
+// Verificar si el usuario es un administrador
+if ($codigoMesa == "admin") {
+    echo '<p class="ningun-pedido">No tienes ningún pedido reciente</p>';
+} else {
+    // Obtener los pedidos realizados por el usuario actual, agrupados por codigo_mesa
+    $stmt = $conn->prepare("SELECT codigo_mesa, GROUP_CONCAT(titulo SEPARATOR ', ') AS titulos, SUM(precio * cantidad) AS total FROM pedidos WHERE codigo_mesa = ? AND TIMESTAMPDIFF(MINUTE, fecha_hora, NOW()) <= 60 GROUP BY codigo_mesa");
+    if ($stmt) {
+        $stmt->bind_param("s", $codigoMesa);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $pedidos = $result->fetch_all(MYSQLI_ASSOC);
+        $stmt->close();
+    } else {
+        $response = "Error al preparar la consulta: " . $conn->error;
+    }
 
-                        if (!empty($pedidos)) {
-                            foreach ($pedidos as $pedido) {
-                                $total = $pedido['precio'] * $pedido['cantidad'];
-                                echo '
-                                <div class="pedido-realizado">
-                                    <h3>' . $pedido['codigo_mesa'] . '</h3>
-                                    <p>Producto: ' . $pedido['titulo'] . '</p>
-                                    <p>Cantidad: ' . $pedido['cantidad'] . '</p>
-                                    <p>Total: ' . $total . '€</p>
-                                    <p>Estado: ' . $pedido['estado_pedido'] . '</p>
-                                </div>
-                                ';
-                            }
-                        } else {
-                            echo '<p>No se han realizado pedidos o han pasado más de 60 minutos.</p>';
-                        }
-                    }
+    if (!empty($pedidos)) {
+        foreach ($pedidos as $pedido) {
+            echo '
+            <div class="pedido-realizado">
+                <h3>' . $pedido['codigo_mesa'] . '</h3>
+                <p>Productos: ' . $pedido['titulos'] . '</p>
+                <p>Total: ' . $pedido['total'] . '€</p>
+            </div>
+            ';
+        }
+    } else {
+        echo '<p>No se han realizado pedidos o han pasado más de 60 minutos.</p>';
+    }
+}
 
-                    $conn->close();
-                ?>
+$conn->close();
+?>
+
             </div>
         </main>
     </div>
